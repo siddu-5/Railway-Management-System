0fec2ceedc3d2dafb2d3f786e4d0b61c8a0061f1
siddu-5 <sidduarnakonda@gmail.com>
2026-06-15 21:55:19 +0530

    Implemented user login and logout system

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect
 from database.db import get_db_connection
+from werkzeug.security import check_password_hash,generate_password_hash
+
 app=Flask(__name__)
+app.secret_key="railway_secret_key"
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -10,7 +13,7 @@ def register():
     if request.method=="POST":
         name=request.form["name"]
         email=request.form["email"]
-        password=request.form['password']
+        password=generate_password_hash(request.form['password'])
         
         conn=get_db_connection()
         cursor=conn.cursor()
@@ -23,6 +26,42 @@ def register():
         
         return "Registration Successfull!"
     return render_template("register.html")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        email=request.form['email']
+        password=request.form['password']
+        
+        conn=get_db_connection()
+        cursor=conn.cursor(dictionary=True)
+        
+        query="select * from users where email=%s"
+        cursor.execute(query,(email,))
+        user=cursor.fetchone()
+        cursor.close()
+        conn.close()
+        
+        if user and check_password_hash(user['password'],password):
+            session['user_id']=user['user_id']
+            session['name']=user['name']
+            return redirect("/dashboard")
+        return "Invalid Email or Password"
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect("/login")
+    return render_template(
+        "dashboard.html",
+        name=session["name"]
+    )
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/login")
 
 if __name__=="__main__":
     app.run(debug=True)
